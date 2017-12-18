83e5ab21f4456d353bede6c4a8a54b829c7efbdb
Paula <paulajulieta@live.com.ar>
2017-12-18 02:16:04 -0300

    Recursos
    Agrego los recursos que utilizare

---
+++ js/modelo/modelo.js
@@ -0,0 +1,36 @@
+/*
+ * Modelo
+ */
+var Modelo = function() {
+  this.preguntas = [];
+  this.ultimoId = 0;
+
+  //inicializacion de eventos
+  this.preguntaAgregada = new Evento(this);
+};
+
+Modelo.prototype = {
+  //se obtiene el id más grande asignado a una pregunta
+  obtenerUltimoId: function() {
+    var maxId = -1;
+    for(var i=0;i<this.preguntas.length;++i){
+      if(this.preguntas[i].id > maxId)
+        maxId = this.preguntas[i].id;
+    }
+    return maxId;
+  },
+
+  //se agrega una pregunta dado un nombre y sus respuestas
+  agregarPregunta: function(nombre, respuestas) {
+    var id = this.obtenerUltimoId();
+    id++;
+    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
+    this.preguntas.push(nuevaPregunta);
+    this.guardar();
+    this.preguntaAgregada.notificar();
+  },
+
+  //se guardan las preguntas
+  guardar: function(){
+  },
+};
